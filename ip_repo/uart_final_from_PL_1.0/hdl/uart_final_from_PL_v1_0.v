23be5494409762b5abfcdf827f72a66cea5648f7
narendiran1996 <narendiran1996@gmail.com>
2020-06-03 11:49:34 +0530

    added files from ip repo

---
+++ ip_repo/uart_final_from_PL_1.0/hdl/uart_final_from_PL_v1_0.v
@@ -0,0 +1,81 @@
+
+`timescale 1 ns / 1 ps
+
+	module uart_final_from_PL_v1_0 #
+	(
+	    parameter UART_TX_FULL_ADDRESS = 32'hE0001010,
+		parameter UART_DATA_TX_RX_ADDRESS = 32'hE0001030,
+		parameter integer C_M00_AXI_ADDR_WIDTH	= 32,
+		parameter integer C_M00_AXI_DATA_WIDTH	= 32
+		
+	)
+	(
+	   output wire [1:0]c_state,
+		   
+        
+		input wire  init_txt,
+		
+		
+		input wire  m00_axi_aclk,
+		input wire  m00_axi_aresetn,
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_awaddr,
+		output wire [2 : 0] m00_axi_awprot,
+		output wire  m00_axi_awvalid,
+		input wire  m00_axi_awready,
+		output wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_wdata,
+		output wire [C_M00_AXI_DATA_WIDTH/8-1 : 0] m00_axi_wstrb,
+		output wire  m00_axi_wvalid,
+		input wire  m00_axi_wready,
+		input wire [1 : 0] m00_axi_bresp,
+		input wire  m00_axi_bvalid,
+		output wire  m00_axi_bready,
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_araddr,
+		output wire [2 : 0] m00_axi_arprot,
+		output wire  m00_axi_arvalid,
+		input wire  m00_axi_arready,
+		input wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_rdata,
+		input wire [1 : 0] m00_axi_rresp,
+		input wire  m00_axi_rvalid,
+		output wire  m00_axi_rready
+	);
+// Instantiation of Axi Bus Interface M00_AXI
+	uart_final_from_PL_v1_0_M00_AXI # ( 	
+		.C_M_AXI_ADDR_WIDTH(C_M00_AXI_ADDR_WIDTH),
+		.C_M_AXI_DATA_WIDTH(C_M00_AXI_DATA_WIDTH),
+		.UART_TX_FULL_ADDRESS(UART_TX_FULL_ADDRESS),
+		.UART_DATA_TX_RX_ADDRESS(UART_DATA_TX_RX_ADDRESS)
+	) uart_final_from_PL_v1_0_M00_AXI_inst (
+	     .c_state(c_state),
+	     
+
+                
+		.init_txt(init_txt),
+		
+		.M_AXI_ACLK(m00_axi_aclk),
+		.M_AXI_ARESETN(m00_axi_aresetn),
+		.M_AXI_AWADDR(m00_axi_awaddr),
+		.M_AXI_AWPROT(m00_axi_awprot),
+		.M_AXI_AWVALID(m00_axi_awvalid),
+		.M_AXI_AWREADY(m00_axi_awready),
+		.M_AXI_WDATA(m00_axi_wdata),
+		.M_AXI_WSTRB(m00_axi_wstrb),
+		.M_AXI_WVALID(m00_axi_wvalid),
+		.M_AXI_WREADY(m00_axi_wready),
+		.M_AXI_BRESP(m00_axi_bresp),
+		.M_AXI_BVALID(m00_axi_bvalid),
+		.M_AXI_BREADY(m00_axi_bready),
+		.M_AXI_ARADDR(m00_axi_araddr),
+		.M_AXI_ARPROT(m00_axi_arprot),
+		.M_AXI_ARVALID(m00_axi_arvalid),
+		.M_AXI_ARREADY(m00_axi_arready),
+		.M_AXI_RDATA(m00_axi_rdata),
+		.M_AXI_RRESP(m00_axi_rresp),
+		.M_AXI_RVALID(m00_axi_rvalid),
+		.M_AXI_RREADY(m00_axi_rready)
+	);
+
+	// Add user logic here
+
+	// User logic ends
+
+	endmodule
